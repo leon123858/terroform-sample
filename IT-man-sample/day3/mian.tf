9f1395e6341e0ccfac12039ad62aa895f7c01616
Leon <a0970785699@gmail.com>
2023-09-12 17:40:37 +0800

    day3

---
+++ IT-man-sample/day3/mian.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_firewall" "rules" {
+  name        = "my-firewall-rule"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["web"]
+}
---
+++ IT-man-sample/day3/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "tw-rd-ca-leon-lin"
+}
+variable "region" {
+  description = "Name of the region."
+  type        = string
+  default     = "asia-east1"
+}
+variable "zone" {
+  description = "Name of the zone_1."
+  type        = string
+  default     = "asia-east1-a"
+}
