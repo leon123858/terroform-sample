06df5a3066076b5d2d13606bd8c9ba5a66d6d715
Leon Lin <56253942+leon123858@users.noreply.github.com>
2023-11-13 15:13:10 +0800

    debug

--- private-gke-mail/infra/jump_gce/main.tf
+++ private-gke-mail/infra/jump_gce/main.tf
@@ -15,9 +15,10 @@ resource "google_compute_instance" "jump" {
     mode = "READ_WRITE"
   }
 
-  can_ip_forward      = false
-  deletion_protection = false
-  enable_display      = false
+  can_ip_forward            = false
+  deletion_protection       = false
+  enable_display            = false
+  allow_stopping_for_update = true
 
   machine_type = "e2-medium"
   name         = var.name
@@ -38,8 +39,16 @@ resource "google_compute_instance" "jump" {
   }
 
   service_account {
-    email  = "${var.project_number}-compute@developer.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+    email = "${var.project_number}-compute@developer.gserviceaccount.com"
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring.write",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
   }
 
   shielded_instance_config {
